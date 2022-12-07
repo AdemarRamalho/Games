26a28c16d4e81886926fe629b7cbf90b41e7ef97
Ademar Ramalho <ademar.neto@descomplica.com.br>
2022-12-06 21:41:10 -0300

    att do codigo

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let botao_pc = document.getElementById("botao_pc");
+botao_pc.value = "PC"
+let botao_browser = document.getElementById("botao_browser");
+botao_browser.value = "Browser"
+let botao_all = document.getElementById("botao_all");
+botao_all.value = "All"
+let botao_fav = document.getElementById("botao_fav");
+botao_fav.value = "Favoritos"
+let btn_home = document.getElementById("btn_home");
+btn_home.value = "HOME"
